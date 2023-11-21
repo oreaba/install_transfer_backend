9a4df55ebf0cda225e53c5f23a926d3d484d04ab
Mohammad H. Oreaba <m.h.oreaba@gmail.com>
2023-11-21 16:49:45 +0200

    fixing errors

--- 5_clone_project.sh
+++ 5_clone_project.sh
@@ -11,18 +11,3 @@ mkdir -p "$TARGET_DIRECTORY"
 # Clone the repository into the target directory
 git clone "$BITBUCKET_REPO" "$TARGET_DIRECTORY" # do not run this using sudo to be the owner	
 # sudo mv ~/zakaa_backend /var/www/
-
-echo "5.2 Installing virtualenvironemnt ..."
-cd /var/www/zakaa_backend/
-ls -ld /var/www/zakaa_backend/
-pip3 install virtualenv
-
-echo "5.3 Installing project dependencies ..."
-virtualenv .venv && source .venv/bin/activate
-which python
-pip3 install -r requirements.txt
-
-echo "5.4 setting project settings and environment variables [.env] ..."
-# copy the environment variable file:
-sudo cp ~/install/env.env /var/www/zakaa_backend/.env
-cat /var/www/zakaa_backend/.env
--- 6_run_project.sh
+++ 6_run_project.sh
@@ -2,20 +2,36 @@
 # read -p "step 6"
 
 # running the project
-echo "6.1 Activating the virtual environment ..."
-cd /var/www/zakaa_backend/
-source .venv/bin/activate
 
-echo "6.2 Applying Database Migrations ..."
+echo "6.1 Installing virtual  environemnt ..."
+# cd /var/www/zakaa_backend/
+# ls -ld /var/www/zakaa_backend/
+pip3 install virtualenv
+
+echo "6.2 Activating virtual environment ..."
+cd /var/www/zakaa_backend/
+virtualenv .venv && source .venv/bin/activate
+which python
+
+echo "6.3 Installing Project Dependencies ..."
+pip3 install -r requirements.txt
+
+echo "6.4 setting project settings and environment variables [.env] ..."
+# copy the environment variable file:
+sudo cp ~/transfer_installer/env.env /var/www/zakaa_backend/.env
+cat /var/www/zakaa_backend/.env
+
+
+echo "6.5 Applying Database Migrations ..."
 python manage.py makemigrations
 python manage.py migrate
 # python manage.py createsuperuser --email mhamdy@aucegypt.edu
 # if you don’t want to enter password now:  
 
-echo "6.3 Creating Superuser ..."
+echo "6.6 Creating Superuser ..."
 python manage.py createsuperuser --email mhamdy@aucegypt.edu --noinput
 
-echo "6.4 Generating Project Static Assets ..."
+echo "6.7 Generating Project Static Assets ..."
 # creating static files for all the project so that they are served by nginx
 python manage.py collectstatic
 # 199 static files copied to '/var/www/zakaa_backend/static', 619 post-processed.
