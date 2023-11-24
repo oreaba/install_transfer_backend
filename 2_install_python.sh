acdb2115843ab8c6c27a5fda4c6efb7e436f1174
Mohammad H. Oreaba <m.h.oreaba@gmail.com>
2023-11-24 20:27:14 +0200

    tesing all scripts

--- 2_install_python.sh
+++ 2_install_python.sh
@@ -6,21 +6,21 @@ echo "2.1 Installing Python 3.9.5 ..."
 if command -v python3.9 &> /dev/null; then
     echo "Python 3.9 is already installed. Skipping compilation."
     exit 0
+else
+    sudo yum install -y gcc openssl-devel bzip2-devel libffi-devel zlib-devel
+    cd /usr/src/
+    sudo wget https://www.python.org/ftp/python/3.9.5/Python-3.9.5.tgz
+    sudo tar xvf Python-3.9.5.tgz
+    cd Python-3.9.5
+    sudo ./configure --enable-optimizations
+    sudo make altinstall
+    sudo ln -s /usr/local/bin/python3.9 /usr/local/bin/python3
+    sudo ln -s /usr/local/bin/pip3.9 /usr/local/bin/pip3
 fi
-
-sudo yum install -y gcc openssl-devel bzip2-devel libffi-devel zlib-devel
-cd /usr/src/
-sudo wget https://www.python.org/ftp/python/3.9.5/Python-3.9.5.tgz
-sudo tar xvf Python-3.9.5.tgz
-cd Python-3.9.5
-sudo ./configure --enable-optimizations
-sudo make altinstall
 
 echo "2.2 Checking Installation ..."
 python3.9 --version
 which python3.9
-sudo ln -s /usr/local/bin/python3.9 /usr/local/bin/python3
-sudo ln -s /usr/local/bin/pip3.9 /usr/local/bin/pip3
 python3 --version
 pip3 --version
 # Alternative Solution:
